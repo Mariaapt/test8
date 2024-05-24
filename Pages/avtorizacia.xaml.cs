1a237fe3b690521ebc9373cd8ad706864eb1dc7b
thekingdom11@mail.ru <thekingdom11@mail.ru>
2024-05-24 14:14:42 +1000

    Добавьте файлы проекта.

---
+++ Pages/avtorizacia.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для avtorizacia.xaml
+    /// </summary>
+    public partial class avtorizacia : Page
+    {
+        public avtorizacia()
+        {
+            InitializeComponent();
+        }
+
+        private void Vhod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Regist_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
